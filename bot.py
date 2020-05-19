c0246d4baa20b2e860aec772362dc9c3faa94861
Joel Schwartz <joel.schwartz1993@gmail.com>
2020-05-19 12:06:13 -0500

    add logging to file

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import discord
 import json
 import os
+import datetime
+import time
 from discord.ext import commands, tasks
 from itertools import cycle
 
@@ -34,16 +36,28 @@ async def change_status():
 @client.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
+        print_log('MissingRequiredArgument', ctx)
         return await ctx.send('Please pass in all required arguments.')
+
     if isinstance(error, commands.CommandNotFound):
-        if ctx.invoked_with.startswith('!'):
-            return
-        return await ctx.send('Invalid Command')
+        print_log('CommandNotFound', ctx)
+
     if isinstance(error, commands.BadArgument):
+        print_log('BadArgument', ctx)
         if ctx.command.qualified_name == 'discover':
             return await ctx.send('Argument must be a digit.')
         else:
             return await ctx.send('Try again')
+
+
+def print_log(error_name: str, ctx):
+    ts = time.time()
+    st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    log_message = f"<{st}> - {error_name} error - {ctx.message.author} : {ctx.message.content}"
+
+    print(log_message)
+    with open("error_logs.txt", "a") as log_file:
+        print(log_message, file=log_file)
 
 
 client.run(load_json('token'))
