a77b51b61533ff9f41a79776dfbd4279a55655b4
Joel <joel.schwartz1993@gmail.com>
2022-12-15 09:48:51 -0500

    send free game update every day

--- bot.py
+++ bot.py
@@ -40,7 +40,8 @@ async def send_free_game_message():
 async def on_ready():
     # change_status.start()
     scheduler.start()
-    scheduler.add_job(send_free_game_message, CronTrigger(day_of_week='thu', hour=11, minute=3, jitter=180, timezone='US/Eastern'))
+    # scheduler.add_job(send_free_game_message, CronTrigger(day_of_week='thu', hour=11, minute=3, jitter=180, timezone='US/Eastern'))
+    scheduler.add_job(send_free_game_message, CronTrigger(hour=11, minute=3, jitter=180, timezone='US/Eastern'))
     print('Bot is ready')
 
 
